c80498d09341916c64fa4d3fd4025d488288b1b3
Pyaesone Khant <pyaepsk@gmail.com>
2023-10-25 22:00:27 +0630

    change baseAPI URL

--- src/app/global/baseApi.js
+++ src/app/global/baseApi.js
@@ -1,10 +1,11 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://needy-watch-production.up.railway.app/api",
+        // baseUrl: "https://needy-watch-production.up.railway.app/api",
         // baseUrl: "http://192.168.0.105:8080/api",
+        baseUrl: "https://expense-tracker-nexcode.up.railway.app/api"
     }),
     tagTypes: ["auth", "records", "categories", "user"],
     endpoints: () => ({}),
